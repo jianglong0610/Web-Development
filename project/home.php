58082426243b22fe1b39f9c5dc3c87f103675646
CHAN JIANG LONG <chanjianglong0610@e.newera.edu.my>
2022-11-22 17:04:25 +0800

    correct my topnav home.html to home.php

--- project/home.php
+++ project/home.php
@@ -19,7 +19,7 @@ include 'check.php'
 
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
 
-            <a class="navbar-brand" href="home.html">Home</a>
+            <a class="navbar-brand" href="home.php">Home</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
